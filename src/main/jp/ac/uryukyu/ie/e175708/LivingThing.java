e48228e4fe434cd9b4e4f1f657d3744ac65dbf05
Masaya Matsukawa <e175708@ie.u-ryukyu.ac.jp>
2017-11-14 02:01:44 +0900

    ステップ2終了後コード

---
+++ src/main/jp/ac/uryukyu/ie/e175708/LivingThing.java
@@ -0,0 +1,39 @@
+package jp.ac.uryukyu.ie.e175708;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String name, int hitPoint, int attack) {
+        this.name = name;
+        this.hitPoint = hitPoint;
+        this.attack = attack;
+        dead = false;
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(LivingThing opponent) {
+        if (!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
